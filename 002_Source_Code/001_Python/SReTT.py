2fd135a7c8e1a1d53f6d2fcca0e9eb74be15694e
ecastanedat <esteban.castanedat@gmail.com>
2023-09-11 10:02:08 -0400

    Development Commit
    - Created mock SReTT.py demo for proposal.
    - Created mock GUI for proposal.

---
+++ 002_Source_Code/001_Python/SReTT.py
@@ -0,0 +1,11 @@
+import sys
+
+print('============================================================================')
+print('                              SReTT v0.1                                    ')
+print(' This is a demo that only prints the passed arguments from the command line.')
+print('============================================================================')
+
+
+
+for arg in sys.argv:
+    print(arg)
